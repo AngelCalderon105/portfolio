2dd525a5f6eda3c2cb63534b71e46d56e3b3e08d
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2024-09-04 05:01:46 -0700

    case sensitive socialbutton fix

--- src/app/_components/socialbutton.tsx
+++ src/app/_components/socialbutton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function Socialbutton( {imageSrc, url, altText}:{imageSrc: string, url: string, altText: string} ) {
+export default function SocialButton( {imageSrc, url, altText}:{imageSrc: string, url: string, altText: string} ) {
   return (
     <div className='w-12 border-2 p-2 rounded-xl border-text border-opacity-10'>
         <Image src= {imageSrc} alt = {altText}>
