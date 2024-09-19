3caf4582920e514319e00badbb6d23a5a7c9a65d
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2024-09-19 03:36:27 -0700

    Project Hover Animation

--- src/app/_components/ProjectCard.tsx
+++ src/app/_components/ProjectCard.tsx
@@ -10,27 +10,29 @@ interface ProjectCardProps {
   githubLink: string;
 }
 
-export default function ProjectCard({projectName, tools, projectImage, description, githubLink} :ProjectCardProps ) {
+export default function ProjectCard({projectName, tools, projectImage, description, githubLink} : ProjectCardProps ) {
   return (
-    <div className='card my-5 p-0 '>
-      <a href={githubLink}className=" flex justify-start gap-2">
-        <Image 
-        src = {projectImage} 
-        alt = "Company Logo"
-        className="rounded-t-3xl"  
-        ></Image>
-      </a>
+    <div className='card my-5 p-0 overflow-hidden group'>
+      <a href={githubLink} className="">
+        <div className="flex justify-start gap-2 overflow-hidden">
+          <Image 
+            src={projectImage} 
+            alt="Company Logo"
+            className="rounded-t-3xl transform transition-transform duration-700 group-hover:scale-105"  
+          />
+        </div>
       
-      <div className="p-5">
-        <p className="text-white text-xl  mb-2">{projectName}</p>
+        <div className="p-5">
+          <p className="text-white text-xl mb-2">{projectName}</p>
           <p className="my-4">{description}</p>
-        <div className="flex flex-wrap gap-2 ">
-        {tools.map(tool => (
-            <TextButton key={tool} text={tool} textSize="text-md"/>
+          <div className="flex flex-wrap gap-2 ">
+            {tools.map(tool => (
+              <TextButton key={tool} text={tool} textSize="text-md"/>
             ))}
+          </div>
         </div>
-        </div>
-    
-      </div>
-  )
+      </a>
+    </div>
+  );
 }
+
